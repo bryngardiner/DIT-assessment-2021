61a6a25342d47b597f43c0e16b920efe8f3b7d9b
bryngardiner <66708233+bryngardiner@users.noreply.github.com>
2021-11-02 11:49:05 +1300

    version one
    has gui setup n shi

---
+++ version one.py
@@ -0,0 +1,16 @@
+# quiz version one 
+# bryn
+
+# import libraries
+from appJar import gui
+
+#functions
+
+# class definitions
+
+questions = [] 
+quiz_progress = 0
+
+# main routine       
+
+quiz = Gui()
